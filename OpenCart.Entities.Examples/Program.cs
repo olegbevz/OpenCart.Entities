0d80a6506d86e4dda3efc1f5fcb3d2a69cf98b2c
olegbevz <olegdb@inbox.ru>
2017-08-30 20:21:48 +0300

    removed test code

--- OpenCart.Entities.Examples/Program.cs
+++ OpenCart.Entities.Examples/Program.cs
@@ -12,24 +12,11 @@ namespace OpenCart.Entities.Examples
             {
                 using (var openCartDomain = new OpenCartDomain())
                 {
-                    var entities = openCartDomain.GetType().Assembly.GetTypes()
-                        .Where(type => !type.IsInterface &&
-                            !type.IsSealed &&
-                            !type.IsAbstract &&
-                            type.GetCustomAttributes(typeof(TableAttribute), false).Length > 0);
-
-                   
-
                     var products = openCartDomain.Products.Include("Manufacturer")
                         .Where(product => product.Manufacturer.Name == "Casio")
                         .ToArray();
 
                     var orders = openCartDomain.Orders.ToArray();
-
-                    foreach (var entity in entities)
-                    {
-                        var set = openCartDomain.Set(typeof(Blog)).AsNoTracking().;
-                    }
                 }
             }
             catch (Exception ex)
